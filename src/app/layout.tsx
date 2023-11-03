872209946b6a3e6080352ff50b5db916c7d55fd2
Marvin <mdengomg2@gmail.com>
2023-11-03 16:48:37 -0700

    Added task grid page and card component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme>
+          {children}
+        </Theme>
+      </body>
     </html>
   )
 }
