ffa8e3d930a4ab53b284e0cbcf8f189c03011a53
Albert Dong <albertd@g.ucla.edu>
2023-11-05 17:45:13 -0800

    Lock /api/tasks POST behind authentication and associate tasks with user

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,6 +1,8 @@
 import { task } from "%/schema";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql);
@@ -15,9 +17,17 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return Response.json({}, { status: 401 });
+  }
+
   try {
     const req = await request.json();
-    const result = await db.insert(task).values(req).returning({ id: task.id });
+    const result = await db
+      .insert(task)
+      .values({ userId: session.user.id, ...req })
+      .returning({ id: task.id });
     return Response.json(result, { status: 200 });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
