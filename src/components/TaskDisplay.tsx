62453473bfc72b16bea4cda9a18e87989227677b
Bill <44715947+certainlynotwrong@users.noreply.github.com>
2023-11-12 11:54:22 -0800

    Masonry layout, resize optimization

--- src/components/TaskDisplay.tsx
+++ src/components/TaskDisplay.tsx
@@ -1,66 +1,62 @@
-import TaskCard from "@/components/TaskCard";
+import TaskCard from '@/components/TaskCard';
+import Layout from '@/app/layout'
 
 const TaskDisplay = () => {
-  const mockData = [
-    {
-      title: "FROG 1",
-      date: "10-23-2023",
-      summary: "This is a frog 1",
-      image_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg",
-    },
-    {
-      title: "FROG 1",
-      date: "10-23-2023",
-      summary: "This is a frog 1",
-      image_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg",
-    },
-    {
-      title: "FROG 1",
-      date: "10-23-2023",
-      summary: "This is a frog 1",
-      image_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg",
-    },
-    {
-      title: "FROG 1",
-      date: "10-23-2023",
-      summary: "This is a frog 1",
-      image_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg",
-    },
-    {
-      title: "FROG 1",
-      date: "10-23-2023",
-      summary: "This is a frog 1",
-      image_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg",
-    },
-    {
-      title: "FROG 1",
-      date: "10-23-2023",
-      summary: "This is a frog 1",
-      image_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg",
-    },
-  ];
+    const mockData = [
+        {
+            title: "iPhone 1",
+            date: "10-23-2023",
+            summary: "This is an iPhone",
+            image_url: "https://upload.wikimedia.org/wikipedia/en/7/79/IOS_17_Homescreen.png"
+        },
+        {
+            title: "FROG 1",
+            date: "10-23-2023",
+            summary: "This is a frog 1",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg"
+        },
+        {
+            title: "FROG 2",
+            date: "10-23-2023",
+            summary: "This is a frog 2",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg"
+        },
+        {
+            title: "FROG 3",
+            date: "10-23-2023",
+            summary: "This is a frog 3",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/6/6c/Renault_Modus_Phase_I_1.6_16V.JPG"
+        },
+        {
+            title: "FROG 4",
+            date: "10-23-2023",
+            summary: "This is a frog 4",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg"
+        },
+        {
+            title: "FROG 5",
+            date: "10-23-2023",
+            summary: "This is a frog 5",
+            image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg"
+        },
+    ];
 
-  return (
-    <div className="max-w-screen-lg mx-auto">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {mockData.map((data, index) => (
-          <TaskCard
-            key={index}
-            title={data.title}
-            date={data.date}
-            summary={data.summary}
-            image_url={data.image_url}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
-
+    return (
+        <Layout>
+            <div className="p-5 sm:p-8">
+                <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4">
+                        {mockData.map((data, index) => (
+                            <TaskCard
+                                key={index}
+                                title={data.title}
+                                date={data.date}
+                                summary={data.summary}
+                                image_url={data.image_url}
+                            />
+                        ))}
+                </div>
+            </div>
+        </Layout>
+    );
+}
 export default TaskDisplay;
