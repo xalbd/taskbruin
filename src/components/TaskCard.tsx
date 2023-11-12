f2848e8e7b7646c4c408900824d9680867c66e47
Bill <44715947+certainlynotwrong@users.noreply.github.com>
2023-11-11 22:34:40 -0800

    Masonry layout, dynamic task card size

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,5 +1,4 @@
-
-import React from 'react'
+import React from 'react';
 
 interface TaskCardProps {
     title: string;
@@ -7,13 +6,16 @@ interface TaskCardProps {
     summary: string;
     image_url: string;
 }
-const TaskCard: React.FC<TaskCardProps> = ({ title, date, summary, image_url }) => {{
+
+const TaskCard: React.FC<TaskCardProps> = ({ title, date, summary, image_url }) => {
     return (
-        <div className="flex-shrink-0 shadow-md rounded-lg overflow-hidden flex p-4">
+        <div className="flex-shrink-0 shadow-md rounded-lg overflow-hidden flex p-4" style={{ width: '100%' }}>
             <div className="w-full p-4">
-                <img src={image_url} 
+                <img 
+                    src={image_url} 
                     alt={"FROG"} 
-                    className="w-full h-48 object-cover mb-2" 
+                    className="w-full mb-2" // Remove object-cover class
+                    style={{ maxWidth: '100%' }} // Set maxWidth to maintain aspect ratio
                 />
                 <p className="mb-2"> {date} </p>
                 <h2 className="text-xl font-bold mb-2">{title}</h2>
@@ -21,6 +23,6 @@ const TaskCard: React.FC<TaskCardProps> = ({ title, date, summary, image_url }) 
             </div>
         </div>
     );
-}}
+}
 
-export default TaskCard
+export default TaskCard;
--- src/pages/TaskDisplay.tsx
+++ src/pages/TaskDisplay.tsx
@@ -1,13 +1,16 @@
+"use client"
 import TaskCard from '@/components/TaskCard';
 import Layout from '@/layout/Layout'
+import Masonry from 'react-masonry-css'
+
 
 const TaskDisplay = () => {
     const mockData = [
         {
-            title: "FROG 1",
+            title: "iPhone 1",
             date: "10-23-2023",
-            summary: "This is a frog 1",
-            image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg"
+            summary: "This is an iPhone",
+            image_url: "https://upload.wikimedia.org/wikipedia/en/7/79/IOS_17_Homescreen.png"
         },
         {
             title: "FROG 1",
@@ -16,51 +19,47 @@ const TaskDisplay = () => {
             image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg"
         },
         {
-            title: "FROG 1",
+            title: "FROG 2",
             date: "10-23-2023",
-            summary: "This is a frog 1",
+            summary: "This is a frog 2",
             image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg"
         },
         {
-            title: "FROG 1",
+            title: "FROG 3",
             date: "10-23-2023",
-            summary: "This is a frog 1",
+            summary: "This is a frog 3",
             image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg"
         },
         {
-            title: "FROG 1",
+            title: "FROG 4",
             date: "10-23-2023",
-            summary: "This is a frog 1",
+            summary: "This is a frog 4",
             image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg"
         },
         {
-            title: "FROG 1",
+            title: "FROG 5",
             date: "10-23-2023",
-            summary: "This is a frog 1",
+            summary: "This is a frog 5",
             image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Frog_on_palm_frond.jpg/1200px-Frog_on_palm_frond.jpg"
         },
     ];
 
     return (
         <Layout>
-            <div className="max-w-screen-lg mx-auto">
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                    {mockData.map((data, index) => (
-                        <TaskCard
-                            key={index}
-                            title={data.title}
-                            date={data.date}
-                            summary={data.summary}
-                            image_url={data.image_url}
-                        />
-                    ))}
+            <div className="p-5 sm:p-8">
+                <div className="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4">
+                        {mockData.map((data, index) => (
+                            <TaskCard
+                                key={index}
+                                title={data.title}
+                                date={data.date}
+                                summary={data.summary}
+                                image_url={data.image_url}
+                            />
+                        ))}
                 </div>
             </div>
         </Layout>
     );
 }
-
-
 export default TaskDisplay;
-
-
