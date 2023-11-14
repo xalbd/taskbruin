fc359e7afe3e665203d7201081867305128cc582
Albert Dong <albertd@g.ucla.edu>
2023-11-14 11:42:25 -0800

    Migrate database for task acceptance

--- db/migrate.ts
+++ db/migrate.ts
@@ -1,4 +1,10 @@
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import { migrate } from "drizzle-orm/neon-http/migrator";
-import db from "@/utils/getDrizzle";
+
+neonConfig.fetchConnectionCache = true;
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql);
 
 migrate(db, { migrationsFolder: "drizzle" });
