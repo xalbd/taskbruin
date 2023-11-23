4851b3842e67b79f74bbeb64a547bdc2ef1e4750
Albert Dong <albertd@g.ucla.edu>
2023-11-23 00:38:50 -0800

    Formatting pass with Prettier

--- src/components/FilterMenu.tsx
+++ src/components/FilterMenu.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React from "react";
 
 interface FilterMenuProps {
   selectedCategories: number[];
   setSelectedCategories: (selectedCategories: number[]) => void;
 }
 
-const categories = ["Laundry", "Food Delivery", "Scooter Rental", "Apartment Listings", "Other"];
+const categories = [
+  "Laundry",
+  "Food Delivery",
+  "Scooter Rental",
+  "Apartment Listings",
+  "Other",
+];
 
-const FilterMenu: React.FC<FilterMenuProps> = ({ selectedCategories, setSelectedCategories }) => {
+const FilterMenu: React.FC<FilterMenuProps> = ({
+  selectedCategories,
+  setSelectedCategories,
+}) => {
   const handleCategoryClick = (index: number) => {
     const isSelected = selectedCategories.includes(index);
 
     if (isSelected) {
-      setSelectedCategories(selectedCategories.filter((selectedIndex) => selectedIndex !== index));
+      setSelectedCategories(
+        selectedCategories.filter((selectedIndex) => selectedIndex !== index),
+      );
     } else {
       setSelectedCategories([...selectedCategories, index]);
     }
@@ -26,8 +37,8 @@ const FilterMenu: React.FC<FilterMenuProps> = ({ selectedCategories, setSelected
             key={index}
             className={`mr-3 mb-2 text-sm px-4 py-2 rounded-full ${
               selectedCategories.includes(index)
-                ? 'border border-blue-500 text-blue-500'
-                : 'border border-gray-400'
+                ? "border border-blue-500 text-blue-500"
+                : "border border-gray-400"
             }`}
             onClick={() => handleCategoryClick(index)}
           >
--- src/components/TaskDisplay.tsx
+++ src/components/TaskDisplay.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import TaskCard from "@/components/TaskCard";
 import useSWR from "swr";
 import SearchBar from "@/components/SearchBar";
-import FilterMenu from "@/components/FilterMenu"
+import FilterMenu from "@/components/FilterMenu";
 
 interface Task {
   id: number;
@@ -27,7 +27,9 @@ const fetcher = async (endpoint: string) => {
 const TaskDisplay = () => {
   const { data, error, isLoading } = useSWR("/api/task", fetcher);
   const [searchString, setSearchString] = React.useState("");
-  const [selectedCategories, setSelectedCategories] = React.useState<number[]>([]);
+  const [selectedCategories, setSelectedCategories] = React.useState<number[]>(
+    [],
+  );
 
   const filterData = () => {
     if (searchString.length !== 0 && data) {
@@ -56,12 +58,12 @@ const TaskDisplay = () => {
           <SearchBar setResults={setSearchString} />
         </div>
         <div className="mt-5">
-          <FilterMenu 
+          <FilterMenu
             selectedCategories={selectedCategories}
             setSelectedCategories={setSelectedCategories}
           />
         </div>
-        
+
         {isLoading && (
           <h1 className="mt-5 text-2xl text-center text-gray-400">
             Loading...
