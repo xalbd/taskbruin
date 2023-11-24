100aa22f97de63da57ae5c61deddeff4428f4e08
Marvin <mdengomg2@gmail.com>
2023-11-24 15:58:32 -0800

    Added task list and profile background image

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -11,8 +11,8 @@ interface UserInfo {
 
 const Profile = () => {
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
-  const [createdTasks, setCreatedTasks] = useState<Task[] | null>(null);
-  const [acceptedTasks, setAcceptedTasks] = useState<Task[] | null>(null);
+  const [createdTasks, setCreatedTasks] = useState<Task[]>([]);
+  const [acceptedTasks, setAcceptedTasks] = useState<Task[]>([]);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -50,28 +50,62 @@ const Profile = () => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center mt-7">
-      <div>
-        <div className="flex items-center justify-center">
-          {userInfo && (
-            <div className="flex">
-              <div className="mr-4">
-                <img
-                  src={userInfo.image}
-                  alt={`Profile picture`}
-                  className="rounded-full w-20 h-20 object-cover"
-                />
-              </div>
-              <div>
-                <h2 className="text-2xl font-bold">{userInfo.name}</h2>
-                <p className="text-lg">{userInfo.email}</p>
+    <div className={`relative min-h-screen mt-10`}>
+      <div
+        className={`relative shadow-lg rounded-lg overflow-hidden`}
+        style={{
+          minHeight: "50vh",
+          width: "75%",
+          margin: "0 auto",
+          position: "relative",
+          boxShadow: "0 4px 6px rgba(100, 149, 237, 0.5)",
+        }}
+      >
+        <div
+          className="absolute top-0 left-0 right-0"
+          style={{
+            backgroundImage:
+              'url("https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2710&q=80")',
+            backgroundSize: "cover",
+            backgroundPosition: "top center",
+            height: "400px",
+            zIndex: 1,
+          }}
+        />
+
+        <div className="flex items-center justify-center mt-7 relative z-10">
+          <div>
+            <div className="flex flex-col items-center justify-center text-white">
+              {userInfo && (
+                <>
+                  <div>
+                    <img
+                      src={userInfo.image}
+                      alt={`Profile picture`}
+                      className="rounded-full max-w-full max-h-full object-cover"
+                      style={{
+                        width: "200px",
+                        height: "200px",
+                      }}
+                    />
+                  </div>
+                  <div className="mt-2 text-center">
+                    <h2 className="text-4xl font-bold mt-4 mb-4">
+                      {userInfo.name}
+                    </h2>
+                    <p className="text-lg">{userInfo.email}</p>
+                  </div>
+                </>
+              )}
+            </div>
+
+            <div className="flex mt-24 mb-10 mr-5 ml-5">
+              <ProfileTasks title={"My Tasks"} tasks={createdTasks} />
+              <div className="ml-9">
+                <ProfileTasks title={"Accepted Tasks"} tasks={acceptedTasks} />
               </div>
             </div>
-          )}
-        </div>
-        <div className="flex mt-4">
-          <ProfileTasks title={"Created Tasks"} tasks={createdTasks} />
-          <ProfileTasks title={"Accepted Tasks"} tasks={acceptedTasks} />
+          </div>
         </div>
       </div>
     </div>
@@ -79,3 +113,4 @@ const Profile = () => {
 };
 
 export default Profile;
+
--- src/components/ProfileTasks.tsx
+++ src/components/ProfileTasks.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Task } from "../../types/task"
+import { Task } from "../../types/task";
 
 interface ProfileTasksProps {
   title: string;
@@ -11,11 +11,43 @@ const ProfileTasks: React.FC<ProfileTasksProps> = ({ title, tasks }) => {
     <div>
       {tasks && (
         <div className="mr-4">
-          <h3 className="text-xl font-bold mb-2">{title}</h3>
+          <h3 className="text-xl font-bold mb-6">{title}</h3>
           {tasks.map((task) => (
-            <div key={task.id}>
-              <div>{task.title}</div>
-              <div>{task.description}</div>
+            <div
+              key={task.id}
+              className={`w-full lg:flex mb-8 `}
+            >
+              <div
+                className="lg:w-64 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                style={{
+                  backgroundImage:
+                    "url('https://tailwindcss.com/img/card-left.jpg')",
+                }}
+                title="Woman holding a mug"
+              ></div>
+              <div
+                className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+              >
+                <div className="mb-8">
+                  <div className="font-bold text-xl mb-2">
+                    {task.title}
+                  </div>
+                  <p className="text-grey-darker text-base">
+                    {task.description}
+                  </p>
+                </div>
+                <div className="flex items-center">
+                  <img
+                    className="w-10 h-10 rounded-full mr-4"
+                    src="https://pbs.twimg.com/profile_images/885868801232961537/b1F6H4KC_400x400.jpg"
+                    alt="Avatar of Jonathan Reinink"
+                  />
+                  <div className="text-sm">
+                    <p className="leading-none">Jonathan Reinink</p>
+                    <p className="text-grey-dark">Aug 18</p>
+                  </div>
+                </div>
+              </div>
             </div>
           ))}
         </div>
