db86b04a8ccd17a7bdc81d88082d48b1b664698d
“ChiweiChen” <chiwei0705@gmail.com>
2023-11-28 21:52:43 -0800

    getting provider buttons

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,9 +3,10 @@
 import { getProviders, signIn } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
+import { useEffect } from "react";
 
-export default async function SignIn() {
-  const providers = await getData();
+export default function SignIn() {
+  const providers = getProviders();
   return (
     <>
       <div>Hi</div>
@@ -18,21 +19,4 @@ export default async function SignIn() {
       ))}
     </>
   );
-}
-
-export async function getData() {
-  const session = await getServerSession(authOptions);
-
-  // If the user is already logged in, redirect.
-  // Note: Make sure not to redirect to the same page
-  // To avoid an infinite loop!
-  if (session) {
-    return { redirect: { destination: "/" } };
-  }
-
-  const providers = await getProviders();
-
-  return {
-    props: { providers: providers ?? [] },
-  };
 }
