a49c96b03a7733ca44b6f922c1b1bff0d5f85f52
“ChiweiChen” <chiwei0705@gmail.com>
2023-11-30 18:26:18 -0800

    functional custom sign in page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -3,10 +3,19 @@
 import { getProviders, signIn } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getOperators } from "drizzle-orm";
 
 export default function SignIn() {
-  const providers = getProviders();
+  const [providers, setProviders] = useState([]);
+  useEffect(() => {
+    const getProvidersAsync = async () => {
+      const providers = await getProviders();
+      setProviders(providers);
+    };
+
+    getProvidersAsync(); // run it, run it
+  }, []);
   return (
     <>
       <div>Hi</div>
