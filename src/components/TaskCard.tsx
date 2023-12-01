7685ee693620d5556e4e46da412980535f72d8a2
Marvin <mdengomg2@gmail.com>
2023-11-30 22:11:34 -0800

    Adjusted the text formatting and added price

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -13,30 +13,33 @@ const TaskCard: React.FC<CardProps> = ({ task, horizontal = false }) => {
       const year = date.getFullYear();
       const month = String(date.getMonth() + 1).padStart(2, "0");
       const day = String(date.getDate()).padStart(2, "0");
-      return `${month}/${day}/${year}`;
+      return `${month}/${day}`;
     }
   };
 
   return (
     <div
       className={`${
-        horizontal ? "flex" : ""
-      } flex-shrink-0 rounded-lg overflow-hidden p-4 mb-11`}
+        horizontal ? "flex" : "flex-col "
+      } rounded-lg overflow-hidden p-8 mb-11`}
       style={{ boxShadow: "0 4px 6px rgba(100, 149, 237, 0.5)" }}
     >
       <img
         src={
           "https://static.demilked.com/wp-content/uploads/2018/03/5aaa1cc04ed34-funny-weird-wtf-stock-photos-19-5a3926af95d9d__700.jpg"
         }
-        className={`w-1/2 ${horizontal ? "mr-4 mb-2" : ""} rounded-lg`}
+        className={`${horizontal ? "w-1/2 mr-4 mb-2" : ""} rounded-lg`}
         style={{ maxWidth: "100%" }}
       />
-      <div className={`w-full ${horizontal ? "" : "ml-4"}`}>
-        <p className="mb-2">
+      <div className={`w-full ${horizontal ? "" : ""}`}>
+        <p className="mb-2 mt-5 italic">
           {formatDateString(task.startDate)} - {formatDateString(task.endDate)}
         </p>
-        <h2 className="text-xl font-bold mb-2">{task.title}</h2>
-        <p>{task.description}</p>
+        <h2 className="text-2xl font-bold mb-2 ">{task.title}</h2>
+        <p className="">{task.description}</p>
+        <p className="font-bold mt-2">
+          Price: {task.price}
+        </p>
       </div>
     </div>
   );
