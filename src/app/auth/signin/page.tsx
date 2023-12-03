e0e34d078e69ee0ad136c15b1856e246a1380f03
Albert Dong <albertd@g.ucla.edu>
2023-12-02 17:00:31 -0800

    Fix TS and useEffect issues
    - TS required typing on the useState for the providers
    - useEffect was being called conditionally due to early return sometimes

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,46 +1,59 @@
 "use client";
 
-import { getProviders, signIn, useSession } from "next-auth/react";
+import {
+  ClientSafeProvider,
+  LiteralUnion,
+  getProviders,
+  signIn,
+  useSession,
+} from "next-auth/react";
 import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
+import { BuiltInProviderType } from "next-auth/providers/index";
 
 export default function SignIn() {
-  const [providers, setProviders] = useState([]);
+  const [providers, setProviders] = useState<Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  > | null>();
   const { status: authStatus } = useSession();
-  if (authStatus == "authenticated") {
-    return redirect("/");
-  }
   useEffect(() => {
     const getProvidersAsync = async () => {
-      const providers = await getProviders();
-      setProviders(providers);
+      const oauthProviders = await getProviders();
+      setProviders(oauthProviders);
     };
 
     getProvidersAsync(); // run it, run it
   }, []);
+
+  if (authStatus == "authenticated") {
+    return redirect("/");
+  }
+
   return (
     <>
       <div className="max-w-md mx-auto mt-8 p-6 flex flex-col items-center">
-        <div class="text-3xl mb-3">Sign In</div>
-        <div class="text-sm text-slate-400">
+        <div className="text-3xl mb-3">Sign In</div>
+        <div className="text-sm text-slate-400">
           Welcome to TaskBruin, our UCLA service platform that connects you with
           Bruins to help with food delivery, laundry, scooter rental, and more.
         </div>
         <br></br>
-        {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <button
-              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-              class={
-                provider.name == "GitHub"
-                  ? "text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2"
-                  : ""
-              }
-            >
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))}
+        {providers &&
+          Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+                className={
+                  provider.name == "GitHub"
+                    ? "text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2"
+                    : ""
+                }
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
       </div>
     </>
   );
