294e48c2ab60404a2b2065c26956de959092ebeb
Marvin <mdengomg2@gmail.com>
2023-12-05 12:51:24 -0800

    Updated landing page and sign in button color themes

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -8,15 +8,15 @@ const Landing = () => {
   const { data: session } = useSession();
 
   return (
-    <div className="tracking-normal text-indigo-400 m-6 bg-cover bg-fixed mt-10">
+    <div className="tracking-normal m-6 bg-cover bg-fixed mt-10">
       <div className="container mx-auto max-w-7xl px-2 mt-6 sm:px-6 lg:px-8 flex flex-wrap items-center">
-        <div className="flex flex-col w-full md:w-1/2 justify-center lg:items-start overflow-y-hidden">
+        <div className="flex flex-col w-full md:w-1/2 justify-center lg:items-start">
           <h1 className="my-4 text-5xl md:text-5xl xl:text-7xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-blue-300 to-green-400">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-green-600">
               TaskBruin
             </span>
           </h1>
-          <p className="leading-normal text-xl md:text-2xl mb-8 text-center md:text-left">
+          <p className="text-indigo-500 leading-normal text-xl md:text-2xl mb-8 text-center md:text-left">
             Welcome to TaskBruin, our UCLA service platform that connects you
             with Bruins to help with food delivery, laundry, scooter rental, and
             more.
--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react";
 const LoginButton = () => {
   return (
     <button
-      className="bg-gradient-to-r from-blue-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+      className="font-bold py-2 px-4 rounded border text-indigo-500 border-indigo-500 focus:ring transform transition hover:scale-105 duration-300 ease-in-out hover:bg-indigo-500 hover:text-white"
       onClick={() => signIn()}
     >
       Sign In
