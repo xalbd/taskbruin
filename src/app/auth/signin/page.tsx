4e5ec85c780adc8c026a547fa32977ad5c2bc8a7
Marvin <mdengomg2@gmail.com>
2023-12-06 00:38:35 -0800

    Added google and github icons to sign in page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -10,20 +10,26 @@ import {
 import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
 import { BuiltInProviderType } from "next-auth/providers/index";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
-export default function SignIn() {
+const SignIn = () => {
   const [providers, setProviders] = useState<Record<
     LiteralUnion<BuiltInProviderType, string>,
     ClientSafeProvider
   > | null>();
   const { status: authStatus } = useSession();
+
   useEffect(() => {
     const getProvidersAsync = async () => {
       const oauthProviders = await getProviders();
       setProviders(oauthProviders);
     };
 
-    getProvidersAsync(); // run it, run it
+    // Check if window is defined before executing client-side code
+    if (typeof window !== "undefined") {
+      getProvidersAsync();
+    }
   }, []);
 
   if (authStatus == "authenticated") {
@@ -33,26 +39,34 @@ export default function SignIn() {
   return (
     <>
       <div className="max-w-md mx-auto mt-8 p-6 flex flex-col items-center">
-        <div className="text-3xl mb-3">Sign In</div>
-        <div className="text-sm text-slate-400">
+        <div className="text-5xl mb-7">Sign In</div>
+        <div className="text-xl text-slate-400">
           Welcome to TaskBruin, our UCLA service platform that connects you with
           Bruins to help with food delivery, laundry, scooter rental, and more.
         </div>
-        <br></br>
+        <br />
         {providers &&
           Object.values(providers).map((provider) => (
             <div key={provider.name}>
               <button
                 onClick={() => signIn(provider.id, { callbackUrl: "/explore" })}
                 className={
-                  "text-black dark:text-white bg-gray-300 hover:bg-gray-200 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:bg-gray-700 dark:hover:bg-gray-800 me-2 mb-2"
+                  "font-bold text-lg py-3 px-6 mb-7 rounded border border-indigo-500 focus:ring transform transition hover:scale-105 duration-300 ease-in-out hover:bg-indigo-500 hover:text-white inline-flex items-center space-x-2"
                 }
               >
-                Sign in with {provider.name}
+                {provider.name.toLowerCase() === "google" && (
+                  <FcGoogle className="w-6 h-6 mr-2" />
+                )}
+                {provider.name.toLowerCase() === "github" && (
+                  <FaGithub className="w-6 h-6 mr-2" />
+                )}
+                <span className="text-lg">Sign in with {provider.name}</span>
               </button>
             </div>
           ))}
       </div>
     </>
   );
-}
+};
+
+export default SignIn;
